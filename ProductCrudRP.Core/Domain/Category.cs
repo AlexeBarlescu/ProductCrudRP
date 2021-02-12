cc26335b2d78a803f81fb6175a0331a202ed0f29
alexebarlescu <alexebarlescu@gmailcom>
2021-02-12 07:52:09 +0000

    The Domain Models

---
+++ ProductCrudRP.Core/Domain/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProductCrudRP.Core.Domain
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Category Name")]
+        [Required(ErrorMessage = "Enter the category name")]
+        public string Name { get; set; }
+
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
---
+++ ProductCrudRP.Core/Domain/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductCrudRP.Core.Domain
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Product Name")]
+        [Required(ErrorMessage = "Tell us your name")]
+        public string Name { get; set; }
+
+        [DisplayName("Product Price")]
+        [Required(ErrorMessage = "Enter the product price")]
+        [Column(TypeName = "decimal(8,2)")]
+        public decimal? Price { get; set; }
+
+        [Required(ErrorMessage = "Select a category")]
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+    }
+}
